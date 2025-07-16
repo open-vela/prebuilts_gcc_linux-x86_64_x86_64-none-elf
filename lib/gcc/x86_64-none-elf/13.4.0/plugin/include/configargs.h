5e9419b54646dde7bb57f7fb11699594cef58e64
Shanmin Zhang <zhangshanmin@xiaomi.com>
2025-07-17 01:48:26 +0800

    GCC: update to version to 13.4.0 (x86_64-none-elf) 20250718
    Signed-off-by: Shanmin Zhang <zhangshanmin@xiaomi.com>

---
+++ lib/gcc/x86_64-none-elf/13.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/shanmin/gcc-build/gcc/configure --prefix=/home/shanmin/gcc-build/out/linux-x86_64/x86_64-none-elf --host=x86_64-unknown-linux-gnu --build=x86_64-unknown-linux-gnu --target=x86_64-none-elf --with-zstd=/home/shanmin/gcc-build/out/linux-x86_64/x86_64-none-elf --with-gmp=/home/shanmin/gcc-build/out/linux-x86_64/x86_64-none-elf --with-mpfr=/home/shanmin/gcc-build/out/linux-x86_64/x86_64-none-elf --with-mpc=/home/shanmin/gcc-build/out/linux-x86_64/x86_64-none-elf --with-gnu-as --with-gnu-ld --disable-werror --disable-shared --disable-nls --disable-threads --disable-tls --with-system-zlib --enable-gnu-unique-object --enable-linker-build-id --with-newlib --with-sysroot=/home/shanmin/gcc-build/out/linux-x86_64/x86_64-none-elf/x86_64-none-elf --with-native-system-header-dir=/include --with-headers=yes --enable-languages=c,c++,lto --enable-cet --enable-multilib --with-multilib-list=m64,m32,mx32 --enable-checking=release";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
